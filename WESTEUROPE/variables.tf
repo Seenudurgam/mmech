0d3a178eaa71eaad12ff24fcb0fb153531217a0b
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-22 16:16:07 +0530

    object & tenant  ID

--- WESTEUROPE/variables.tf
+++ WESTEUROPE/variables.tf
@@ -1,3 +1,15 @@
+variable "object_id" {
+  type        = string
+  description = "object id"
+  default = "1c584611-915e-4307-a62a-36f4ff1828fd"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "tenant id"
+  default = "ac67e308-1f19-4011-9baa-c4df0351e262"
+}
+
 variable "resource_group_name" {
   type        = string
   description = "The default Azure region for the resource provisioning"
