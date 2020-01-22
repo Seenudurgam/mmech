c6fd6992fde45e134c3d75f53434ad7e74cee34c
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-22 14:33:50 +0530

    Update variables.tf

--- WESTEUROPE/variables.tf
+++ WESTEUROPE/variables.tf
@@ -1,13 +1,13 @@
 variable "resource_group_name" {
   type        = string
   description = "The default Azure region for the resource provisioning"
-  default = "mmech-ugm-dev-ece-rg"
+  default = "mmech-rg"
 }
 
 variable "resource_group_name_common" {
   type        = string
   description = "The default Azure region for the resource provisioning"
-  default = "mmech-ugm-dev-common-ece-rg"
+  default = "mmech-ugm-rg"
 }
 
 variable "resource_group_location" {
@@ -15,42 +15,11 @@ variable "resource_group_location" {
   description = "The default Azure region for the resource provisioning"
   default = "West Europe"
 }
+variable "appinsight" {
+type        = "string"
+description = "Enter the Application insight name"
+default = "mmech_dev_ece"
 
-variable "subscription_id" {
-  type        = string
-  description = "Enter Subscription ID for provisioning resources in Azure."
-  default = "XYXYX-SubscriptionID-XYXYX"
-}
-
-variable "client_id" {
-  type        = string
-  description = "Enter Client ID for Application created in Azure AD"
-  default = "XYXYX-ClientID-XYXYX"
-}
-
-variable "client_secret" {
-  type        = string
-  description = "Enter Client secret for Application in Azure AD"
-  default = "XYXYX-ClientSecret-XYXYX"
-}
-
-variable "tenant_id" {
-  type        = string
-  description = "Enter Tenant ID / Directory ID of your Azure AD. Run Get-AzureSubscription to know your Tenant ID"
-  default = "XYXYX-TenantID-XYXYX"
-}
-
-variable "object_id" {
-  type        = string
-  description = "Enter Object ID of a user or a service principal in Azure Active Directory"
-  default = "XYXYX-ObjectID-XYXYX"
-}
-
-/*variable "appinsight" {
-  type        = "string"
-  description = "Enter the Application insight name"
-  default = "mmech_ugmappinsights_dev_ece"
-}*/
 
 variable "storageName" {
   type        = string
@@ -61,23 +30,23 @@ variable "storageName" {
 variable "sqlserver" {
   type        = string
   description = "Enter the Sql Server name"
-  default = "mmech-ugmsqlsrv-dev-ece"
+  default = "mmev-ece"
 }
 
 variable "db" {
   type        = string
   description = "Enter the Data base name"
-  default = "mmech-ugmdb-dev-ece"
+  default = "mmechv-ece"
 }
 
 variable "kv" {
   type        = string
   description = "Enter the Key Vault name"
-  default = "mmech-ugmkv-dev-ece"
+  default = "mmech-ece"
 }
 
 variable "acr" {
   type        = string
   description = "Enter the Container Registry name (Unique)"
-  default = "mmechugmacrdevece"
+  default = "mmeece"
 }
