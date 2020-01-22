93cb7c55936ea857d7e3ccdde77b9376a1f9c2b9
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-22 14:35:50 +0530

    Update main.tf

--- WESTEUROPE/main.tf
+++ WESTEUROPE/main.tf
@@ -7,11 +7,7 @@ provider "azurerm" {
  # More information on the authentication methods supported by
 # the AzureRM Provider can be found here:
 # http://terraform.io/docs/providers/azurerm/index.html
-   
-   subscription_id = var.subscription_id
-   client_id       = var.client_id
-   client_secret   = var.client_secret
-   tenant_id       = var.tenant_id
+
  }
 
  # Create a resource group
